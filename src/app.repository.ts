5843928876d389070dce88ba5b6fa679cb3a6275
volodialishchyshyn <lishchishin.volodya@gmail.com>
2023-04-07 00:15:47 +0300

    HW13 Fixed: fixed test/delete-all endpoint

--- src/app.repository.ts
+++ src/app.repository.ts
@@ -3,15 +3,22 @@ import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from './Schemas/user.schema';
 import { Model } from 'mongoose';
 import { Helpers } from './Helpers/helpers';
+import {Post, PostDocument} from "./Schemas/post.schema";
+import {Blog, BlogDocument} from "./Schemas/blog.schema";
 
 @Injectable()
 export class AppRepository {
   constructor(
     @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Post.name) private postModel: Model<PostDocument>,
+    @InjectModel(Blog.name) private blogModel: Model<BlogDocument>,
     public helpers: Helpers,
   ) {}
 
   deleteAll() {
-    return this.userModel.deleteMany({}).exec();
+     this.userModel.deleteMany({}).exec();
+     this.postModel.deleteMany({}).exec();
+     this.blogModel.deleteMany({}).exec();
+     return
   }
 }
