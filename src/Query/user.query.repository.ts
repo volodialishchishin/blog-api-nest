1ec4613f2d9512397aa375f3eaacc4a7aafe6be4
volodialishchyshyn <lishchishin.volodya@gmail.com>
2023-07-02 19:11:32 +0300

    Revert "HW14-1 Part: change config"
    This reverts commit 1bf3f9dee48278060fbf3d3e44b55e177197a144.

--- src/Query/user.query.repository.ts
+++ src/Query/user.query.repository.ts
@@ -19,6 +19,7 @@ export class UserQueryRepository {
     pageSize = 10,
     sortDirection: 'asc' | 'desc' = 'desc',
   ): Promise<UserViewModelWithQuery> {
+    const sortByWithData = `accountData.${sortBy}`
     const matchedUsersWithSkip = await this.userModel
       .find({
         $or: [
@@ -36,7 +37,7 @@ export class UserQueryRepository {
       })
       .skip((pageNumber - 1) * pageSize)
       .limit(Number(pageSize))
-      .sort([[sortBy, sortDirection]])
+      .sort([[sortByWithData, sortDirection]])
       .exec();
     const matchedUsers = await this.userModel
       .find({
