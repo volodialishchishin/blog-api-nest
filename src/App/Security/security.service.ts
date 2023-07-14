7c80ff7e0b1857e1d2a886949f317a4b88cc32dd
volodialishchyshyn <lishchishin.volodya@gmail.com>
2023-07-14 05:53:18 +0300

    HW14-3: verify to verifyasync fix

--- src/App/Security/security.service.ts
+++ src/App/Security/security.service.ts
@@ -8,15 +8,15 @@ export class securityService {
                     private readonly securityRepository:securityRepository ) {
     }
     async getSessions(refreshToken: string) {
-        const {user } = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
+        const { user, deviceId } =  await this.jwtService.verifyAsync<{user:string, deviceId:string}>(refreshToken, {secret: process.env.SECRET || "Ok" })
         return await this.securityRepository.getSessions(user)
     }
     async deleteSessions(refreshToken: string) {
-        const {user, deviceId} = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
+        const { user, deviceId } =  await this.jwtService.verifyAsync<{user:string, deviceId:string}>(refreshToken, {secret: process.env.SECRET || "Ok" })
         return await this.securityRepository.deleteSessions(user, deviceId)
     }
     async deleteSession(refreshToken: string, id: string): Promise<any> {
-        const {user } = <jwt.UserIDJwtPayload>jwt.verify(refreshToken, process.env.SECRET || 'Ok')
+        const { user, deviceId } =  await this.jwtService.verifyAsync<{user:string, deviceId:string}>(refreshToken, {secret: process.env.SECRET || "Ok" })
         try {
             await this.securityRepository.deleteSession(user,id)
         }
