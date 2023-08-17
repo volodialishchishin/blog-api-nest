9557dca84e2a1e6cde4f2259e299b2b6208c5b45
volodialishchyshyn <lishchishin.volodya@gmail.com>
2023-08-17 09:36:51 +0300

    HW18 zdarova 312

--- src/App/Users/user.repository.ts
+++ src/App/Users/user.repository.ts
@@ -158,6 +158,7 @@ export class UserRepository {
     blogId: string,
     banReason: string,
     banDate: string,
+    status:boolean
   ) {
     const user = await this.getUserById(userId);
     if (!user) return null;
@@ -184,7 +185,7 @@ export class UserRepository {
     }
     else {
       const updateUserBanQuery = `
-      delete from user_blogs_ban_entity where id= $1 and "blogId" = $2`;
+      update user_blogs_ban_entity set "banReason" where id= $1 and "blogId" = $2`;
 
       const updateUserBanValues = [userBan.userId, userBan.blogId];
       await this.dataSource.query(updateUserBanQuery, updateUserBanValues);
